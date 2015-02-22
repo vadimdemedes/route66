b7113f7ac5f36cb436a4098e5c6b5c312fdd2d20
vdemedes <vdemedes@gmail.com>
2015-02-22 14:35:46 +0200

    switch from 6to5 to babel

--- build/router.js
+++ build/router.js
@@ -1,25 +1,8 @@
 "use strict";
 
-var _slicedToArray = function (arr, i) {
-  if (Array.isArray(arr)) {
-    return arr;
-  } else {
-    var _arr = [];
+var _slicedToArray = function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { var _arr = []; for (var _iterator = arr[Symbol.iterator](), _step; !(_step = _iterator.next()).done;) { _arr.push(_step.value); if (i && _arr.length === i) break; } return _arr; } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } };
 
-    for (var _iterator = arr[Symbol.iterator](), _step; !(_step = _iterator.next()).done;) {
-      _arr.push(_step.value);
-
-      if (i && _arr.length === i) break;
-    }
-
-    return _arr;
-  }
-};
-
-var _prototypeProperties = function (child, staticProps, instanceProps) {
-  if (staticProps) Object.defineProperties(child, staticProps);
-  if (instanceProps) Object.defineProperties(child.prototype, instanceProps);
-};
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
 /**
  * Dependencies
@@ -32,13 +15,14 @@ var parse = require("url").parse;
 
 require("./util");
 
-
 /**
  * Router
  */
 
 var Router = (function () {
   function Router() {
+    _classCallCheck(this, Router);
+
     this.routes = [];
 
     // private variables
@@ -47,357 +31,303 @@ var Router = (function () {
     this._path = [];
   }
 
-  _prototypeProperties(Router, null, {
-    setup: {
-      value: function setup(routes) {
-        var _this = this;
-        routes.call(this);
+  Router.prototype.setup = function setup(routes) {
+    var _this = this;
 
-        methods.forEach(function (method) {
-          _this._compiledRoutes[method] = [];
-        });
+    routes.call(this);
 
-        this.routes.forEach(function (route) {
-          var _route$target$split = route.target.split("#");
+    methods.forEach(function (method) {
+      _this._compiledRoutes[method] = [];
+    });
 
-          var _route$target$split2 = _slicedToArray(_route$target$split, 2);
+    this.routes.forEach(function (route) {
+      var _route$target$split = route.target.split("#");
 
-          var namespace = _route$target$split2[0];
-          var method = _route$target$split2[1];
+      var _route$target$split2 = _slicedToArray(_route$target$split, 2);
 
+      var namespace = _route$target$split2[0];
+      var method = _route$target$split2[1];
 
-          namespace = namespace.split("/");
-          var controller = namespace.pop();
-          namespace = namespace.join("/");
+      namespace = namespace.split("/");
+      var controller = namespace.pop();
+      namespace = namespace.join("/");
 
-          var target = route.target;
-          var path = route.path;
+      var target = route.target;
+      var path = route.path;
 
-          var keys = [];
-          var regexp = pathToRegexp(route.path, keys);
-          keys = keys.map(function (key) {
-            return key.name;
-          });
+      var keys = [];
+      var regexp = pathToRegexp(route.path, keys);
+      keys = keys.map(function (key) {
+        return key.name;
+      });
 
-          _this._compiledRoutes[route.method].push({
-            path: path,
-            regexp: regexp,
-            keys: keys,
-            namespace: namespace,
-            controller: controller,
-            method: method,
-            target: target
-          });
-        });
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    match: {
-      value: function match(method, path, target) {
-        var options = arguments[3] === undefined ? {} : arguments[3];
-        if (!target) {
-          throw new Error("Target was not specified for route " + method.toUpperCase() + " " + path);
-        }
+      _this._compiledRoutes[route.method].push({
+        path: path,
+        regexp: regexp,
+        keys: keys,
+        namespace: namespace,
+        controller: controller,
+        method: method,
+        target: target
+      });
+    });
+  };
 
-        var namespaces = this._namespaces;
-        var fullPath = this._path;
+  Router.prototype.match = function match(method, path, target) {
+    var options = arguments[3] === undefined ? {} : arguments[3];
 
-        // remove slashes at the start and end
-        path = path.replace(/^\/|\/$/g, "");
-
-        // if route is on collection
-        // remove last component in path
-        // /posts/:post_id => /posts
-        if (options.on === "collection") {
-          var lastComponent = fullPath.pop();
-        }
-
-        // insert path component at the end
-        fullPath.push(path);
-
-        // if there are namespaces
-        // append them to the target
-        if (namespaces.length) {
-          var namespace = namespaces.join("/");
-          target = "" + namespace + "/" + target;
-        }
-
-        // build a path and prepend a slash
-        path = "/" + fullPath.join("/");
-
-        var route = { method: method, path: path, target: target };
-
-        // add route to the collection
-        this.routes.push(route);
-
-        // after route was added
-        // remove its component from path
-        fullPath.length--;
-
-        // route is on collection
-        // so removed component needs to be put back
-        // /posts => /posts/:post_id
-        if (lastComponent) {
-          fullPath.push(lastComponent);
-        }
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    root: {
-      value: function root(target) {
-        this.match("get", "/", target);
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    get: {
-      value: function get(path, target, options) {
-        this.match("get", path, target, options);
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    post: {
-      value: function post(path, target, options) {
-        this.match("post", path, target, options);
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    put: {
-      value: function put(path, target, options) {
-        this.match("put", path, target, options);
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    "delete": {
-      value: function _delete(path, target, options) {
-        this.match("delete", path, target, options);
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    namespace: {
-      value: function namespace(path, routes) {
-        var namespaces = this._namespaces;
-        var fullPath = this._path;
-
-        // remove slashes at the start and end
-        path = path.replace(/^\/|\/$/g, "");
-
-        // add path component
-        fullPath.push(path);
-
-        // keeping track of all namespaces
-        namespaces.push(path);
-
-        routes.call(this);
-
-        // remove namespace name from path
-        fullPath.pop();
-
-        // namespace is no longer needed, remove
-        namespaces.length--;
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    resource: {
-      value: function resource(name, options, routes) {
-        var options = arguments[1] === undefined ? {} : arguments[1];
-        var fullPath = this._path;
-
-        if ("undefined" === typeof routes) {
-          routes = emptyFunction;
-        }
-
-        if ("function" === typeof options) {
-          routes = options;
-          options = {};
-        }
-
-        var pluralName = inflect.pluralize(name);
-        var singularName = inflect.singularize(name);
-
-        var except = options.except || [];
-        var only = options.only || [];
-
-        var _methods = ["index", "new", "edit", "show", "create", "update", "destroy"];
-
-        if (only.length) {
-          _methods = _methods.filter(function (method) {
-            return only.includes(method);
-          });
-        }
-
-        if (except.length) {
-          _methods = _methods.filter(function (method) {
-            return !except.includes(method);
-          });
-        }
-
-        // if it's a collection resource
-        // remove :id part of previous resource
-        // and put it back later
-        if (options.on === "collection") {
-          var lastComponent = fullPath.pop();
-        }
-
-        // define CRUD methods
-        if (_methods.includes("index")) this.get(pluralName, "" + pluralName + "#index");
-        if (_methods.includes("new")) this.get("" + pluralName + "/new", "" + pluralName + "#new");
-        if (_methods.includes("edit")) this.get("" + pluralName + "/:" + singularName + "_id/edit", "" + pluralName + "#edit");
-        if (_methods.includes("show")) this.get("" + pluralName + "/:" + singularName + "_id", "" + pluralName + "#show");
-        if (_methods.includes("create")) this.post(pluralName, "" + pluralName + "#create");
-        if (_methods.includes("update")) this.put("" + pluralName + "/:" + singularName + "_id", "" + pluralName + "#update");
-        if (_methods.includes("destroy")) this["delete"]("" + pluralName + "/:" + singularName + "_id", "" + pluralName + "#destroy");
-
-        // add /resource/:resource_id to the path
-        // add separately, because :resource_id
-        // component may be removed by nested collection routes
-        fullPath.push(pluralName, ":" + singularName + "_id");
-
-        // define nested routes
-        routes.call(this);
-
-        // remove /resource/:resource_id
-        // that was added previously
-        fullPath.length -= 2;
-
-        // if it was a collection resource
-        // put back :resource_id of a previous resource
-        if (lastComponent) {
-          fullPath.push(lastComponent);
-        }
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    dispatch: {
-      value: function dispatch(fn) {
-        this._dispatch = fn;
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    express: {
-      value: function express() {
-        var router = this;
-
-        return function (req, res, next) {
-          if (!router._dispatch) {
-            throw new Error("Router does not have a dispatch function.");
-          }
-
-          var result = router.resolve(req.method, req.url);
-
-          if (!result) {
-            res.status = 404;
-            next(false);
-            return;
-          }
-
-          var route = result.route;
-          var params = result.params;
-
-
-          req.params = params;
-
-          router._dispatch(route, req, res, next);
-        };
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    koa: {
-      value: function koa() {
-        var router = this;
-
-        return function* (next) {
-          if (!router._dispatch) {
-            throw new Error("Router does not have a dispatch function.");
-          }
-
-          var result = router.resolve(this.method, this.url);
-
-          if (!result) {
-            this.status = 404;
-            yield next;
-            return;
-          }
-
-          var route = result.route;
-          var params = result.params;
-
-
-          this.params = params;
-
-          yield router._dispatch.call(this, route, this);
-        };
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    resolve: {
-      value: function resolve(method, url) {
-        method = method.toLowerCase();
-
-        var routes = this._compiledRoutes[method];
-
-        var route = undefined;
-        var index = 0;
-
-        url = parse(url).pathname;
-
-        while (route = routes[index++]) {
-          var _ret = (function () {
-            var params = undefined;
-
-            if (params = route.regexp.exec(url)) {
-              // extract parameters from .exec() result
-              // and decodeURIComponent each of them
-              params = params.slice(1).map(decodeURIComponent);
-
-              // assign keys to params array
-              route.keys.forEach(function (key, i) {
-                return params[key] = params[i];
-              });
-
-              return {
-                v: { route: route, params: params }
-              };
-            }
-          })();
-
-          if (typeof _ret === "object") return _ret.v;
-        }
-      },
-      writable: true,
-      enumerable: true,
-      configurable: true
+    if (!target) {
+      throw new Error("Target was not specified for route " + method.toUpperCase() + " " + path);
     }
-  });
+
+    var namespaces = this._namespaces;
+    var fullPath = this._path;
+
+    // remove slashes at the start and end
+    path = path.replace(/^\/|\/$/g, "");
+
+    // if route is on collection
+    // remove last component in path
+    // /posts/:post_id => /posts
+    if (options.on === "collection") {
+      var lastComponent = fullPath.pop();
+    }
+
+    // insert path component at the end
+    fullPath.push(path);
+
+    // if there are namespaces
+    // append them to the target
+    if (namespaces.length) {
+      var namespace = namespaces.join("/");
+      target = "" + namespace + "/" + target;
+    }
+
+    // build a path and prepend a slash
+    path = "/" + fullPath.join("/");
+
+    var route = { method: method, path: path, target: target };
+
+    // add route to the collection
+    this.routes.push(route);
+
+    // after route was added
+    // remove its component from path
+    fullPath.length--;
+
+    // route is on collection
+    // so removed component needs to be put back
+    // /posts => /posts/:post_id
+    if (lastComponent) {
+      fullPath.push(lastComponent);
+    }
+  };
+
+  Router.prototype.root = function root(target) {
+    this.match("get", "/", target);
+  };
+
+  Router.prototype.get = function get(path, target, options) {
+    this.match("get", path, target, options);
+  };
+
+  Router.prototype.post = function post(path, target, options) {
+    this.match("post", path, target, options);
+  };
+
+  Router.prototype.put = function put(path, target, options) {
+    this.match("put", path, target, options);
+  };
+
+  Router.prototype["delete"] = function _delete(path, target, options) {
+    this.match("delete", path, target, options);
+  };
+
+  Router.prototype.namespace = function namespace(path, routes) {
+    var namespaces = this._namespaces;
+    var fullPath = this._path;
+
+    // remove slashes at the start and end
+    path = path.replace(/^\/|\/$/g, "");
+
+    // add path component
+    fullPath.push(path);
+
+    // keeping track of all namespaces
+    namespaces.push(path);
+
+    routes.call(this);
+
+    // remove namespace name from path
+    fullPath.pop();
+
+    // namespace is no longer needed, remove
+    namespaces.length--;
+  };
+
+  Router.prototype.resource = function resource(name, _x, routes) {
+    var options = arguments[1] === undefined ? {} : arguments[1];
+
+    var fullPath = this._path;
+
+    if ("undefined" === typeof routes) {
+      routes = emptyFunction;
+    }
+
+    if ("function" === typeof options) {
+      routes = options;
+      options = {};
+    }
+
+    var pluralName = inflect.pluralize(name);
+    var singularName = inflect.singularize(name);
+
+    var except = options.except || [];
+    var only = options.only || [];
+
+    var methods = ["index", "new", "edit", "show", "create", "update", "destroy"];
+
+    if (only.length) {
+      methods = methods.filter(function (method) {
+        return only.includes(method);
+      });
+    }
+
+    if (except.length) {
+      methods = methods.filter(function (method) {
+        return !except.includes(method);
+      });
+    }
+
+    // if it's a collection resource
+    // remove :id part of previous resource
+    // and put it back later
+    if (options.on === "collection") {
+      var lastComponent = fullPath.pop();
+    }
+
+    // define CRUD methods
+    if (methods.includes("index")) this.get(pluralName, "" + pluralName + "#index");
+    if (methods.includes("new")) this.get("" + pluralName + "/new", "" + pluralName + "#new");
+    if (methods.includes("edit")) this.get("" + pluralName + "/:" + singularName + "_id/edit", "" + pluralName + "#edit");
+    if (methods.includes("show")) this.get("" + pluralName + "/:" + singularName + "_id", "" + pluralName + "#show");
+    if (methods.includes("create")) this.post(pluralName, "" + pluralName + "#create");
+    if (methods.includes("update")) this.put("" + pluralName + "/:" + singularName + "_id", "" + pluralName + "#update");
+    if (methods.includes("destroy")) this["delete"]("" + pluralName + "/:" + singularName + "_id", "" + pluralName + "#destroy");
+
+    // add /resource/:resource_id to the path
+    // add separately, because :resource_id
+    // component may be removed by nested collection routes
+    fullPath.push(pluralName, ":" + singularName + "_id");
+
+    // define nested routes
+    routes.call(this);
+
+    // remove /resource/:resource_id
+    // that was added previously
+    fullPath.length -= 2;
+
+    // if it was a collection resource
+    // put back :resource_id of a previous resource
+    if (lastComponent) {
+      fullPath.push(lastComponent);
+    }
+  };
+
+  Router.prototype.dispatch = function dispatch(fn) {
+    this._dispatch = fn;
+  };
+
+  Router.prototype.express = function express() {
+    var router = this;
+
+    return function (req, res, next) {
+      if (!router._dispatch) {
+        throw new Error("Router does not have a dispatch function.");
+      }
+
+      var result = router.resolve(req.method, req.url);
+
+      if (!result) {
+        res.status = 404;
+        next(false);
+        return;
+      }
+
+      var route = result.route;
+      var params = result.params;
+
+      req.params = params;
+
+      router._dispatch(route, req, res, next);
+    };
+  };
+
+  Router.prototype.koa = function koa() {
+    var router = this;
+
+    return function* (next) {
+      if (!router._dispatch) {
+        throw new Error("Router does not have a dispatch function.");
+      }
+
+      var result = router.resolve(this.method, this.url);
+
+      if (!result) {
+        this.status = 404;
+        yield next;
+        return;
+      }
+
+      var route = result.route;
+      var params = result.params;
+
+      this.params = params;
+
+      yield router._dispatch.call(this, route, this);
+    };
+  };
+
+  Router.prototype.resolve = function resolve(method, url) {
+    method = method.toLowerCase();
+
+    var routes = this._compiledRoutes[method];
+
+    var route = undefined;
+    var index = 0;
+
+    url = parse(url).pathname;
+
+    while (route = routes[index++]) {
+      var _ret = (function () {
+        var params = undefined;
+
+        if (params = route.regexp.exec(url)) {
+          // extract parameters from .exec() result
+          // and decodeURIComponent each of them
+          params = params.slice(1).map(decodeURIComponent);
+
+          // assign keys to params array
+          route.keys.forEach(function (key, i) {
+            return params[key] = params[i];
+          });
+
+          return {
+            v: { route: route, params: params }
+          };
+        }
+      })();
+
+      if (typeof _ret === "object") {
+        return _ret.v;
+      }
+    }
+  };
 
   return Router;
 })();
 
-var emptyFunction = function () {};
-
+var emptyFunction = function emptyFunction() {};
 
 /**
  * Expose Router
